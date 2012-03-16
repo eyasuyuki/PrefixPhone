caca7625514988acc58fa0d3e75bda0f3713f1fc
ENDO Yasuyuki <eyasuyuki@gmail.com>
2012-03-16 16:08:51 +0900

    get prefix from preference remove left zero

--- PrefixPhone/src/org/javaopen/prefix/phone/PrefixActivity.java
+++ PrefixPhone/src/org/javaopen/prefix/phone/PrefixActivity.java
@@ -2,8 +2,10 @@ package org.javaopen.prefix.phone;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.telephony.PhoneNumberUtils;
 
 public class PrefixActivity extends Activity {
@@ -14,9 +16,22 @@ public class PrefixActivity extends Activity {
 
 		Intent intent = getIntent();
 		String number = PhoneNumberUtils.getNumberFromIntent(intent, this);
+		
+		SharedPreferences sp =
+				PreferenceManager.getDefaultSharedPreferences(this);
+		String defValue = getString(R.string.prefix_default);
+		String key = getString(R.string.prefix_key);
+		String prefix = sp.getString(key, defValue);
+		
+		key = getString(R.string.remove_left_key);
+		if (sp.getBoolean(key, false)) {
+			if (number.startsWith("0")) {
+				number = number.substring(1);
+			}
+		}
 
 		Intent callIntent =
-				new Intent(Intent.ACTION_CALL, Uri.parse("tel://003766" + number));
+				new Intent(Intent.ACTION_CALL, Uri.parse("tel://" + prefix + number));
 		startActivity(callIntent);
 	}
 }
